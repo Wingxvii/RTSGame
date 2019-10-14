6d400782892370e7e15deb6252b643fa97298879
John <john.wang@uoit.net>
2019-10-14 18:13:25 -0400

    cleaned up and fixed selection logic

--- Assets/Scripts/SelectionManager.cs
+++ Assets/Scripts/SelectionManager.cs
@@ -10,16 +10,6 @@ public enum MouseEvent
     PrefabBuild = 2,
 }
 
-
-//SelectionManager.Instance.doSomething();
-
-//void SaveToFile(Vector4(objType, x,y,z));
-//Vector4(objType, x,y,z) LoadFromFile();
-
-//SaveManager.Instacne.SaveToFile(fileData);
-
-
-
 public class SelectionManager : MonoBehaviour
 {
     #region SingletonCode
@@ -68,141 +58,11 @@ public class SelectionManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //handle selection box first
+        HandleSelectionBox();
 
-        #region selection box
-        if (boxActive && currentEvent == MouseEvent.PrefabBuild) {
-            boxActive = false;
-            boxStart = Vector2.zero;
-            boxEnd = Vector2.zero;
-        }
-
-        if (Input.GetMouseButtonDown(0) && boxActive == false && currentEvent != MouseEvent.PrefabBuild)
-        {
-            boxStart = Input.mousePosition;
-            boxActive = true;
-        }
-        else if (Input.GetMouseButton(0) && boxActive)
-        {
-            if (Mathf.Abs(boxStart.x - Input.mousePosition.x) > 15 || Mathf.Abs(boxStart.y - Input.mousePosition.y) > 15)
-            {
-                boxEnd = Input.mousePosition;
-            }
-            else
-            {
-                boxEnd = Vector2.zero;
-            }
-        }
-        if (Input.GetMouseButtonUp(0) && boxActive)
-        {
-            Vector3 worldSelection1;
-
-            Ray rayCast = Camera.main.ScreenPointToRay(boxStart);
-            RaycastHit castHit;
-            if (Physics.Raycast(rayCast, out castHit, 500))
-            {
-                worldSelection1 = castHit.point;
-
-                foreach (GameObject obj in AllObjects)
-                {
-                    if (obj.GetComponent<Transform>().position.x >= Mathf.Min(worldSelection1.x, mousePosition.x) &&
-                        obj.GetComponent<Transform>().position.x <= Mathf.Max(worldSelection1.x, mousePosition.x) &&
-                        obj.GetComponent<Transform>().position.z >= Mathf.Min(worldSelection1.z, mousePosition.z) &&
-                        obj.GetComponent<Transform>().position.z <= Mathf.Max(worldSelection1.z, mousePosition.z) && !SelectedObjects.Contains(obj))
-                    {
-                        Debug.Log(obj.name);
-                        SelectedObjects.Add(obj);
-                        currentEvent = MouseEvent.Selection;
-                        obj.GetComponent<SelectableObject>().OnSelect();
-
-                    }
-                }
-            }
-
-            boxStart = Vector2.zero;
-            boxEnd = Vector2.zero;
-            boxActive = false;
-        }
-        #endregion
-
-
-        #region update mouse
-        //update mouse position on screen
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-
-        if (Physics.Raycast(ray, out hit, 500) && hit.transform.gameObject.tag != "SelectableObject")
-        {
-            mousePosition = hit.point;
-            //mousePosition = new Vector3(hit.point.x, 2, hit.point.z);
-        }
-
-        #endregion
-
-        #region place prefab
-        if (currentEvent == MouseEvent.PrefabBuild && Input.GetMouseButtonDown(0))
-        {
-            if (!Input.GetKey(KeyCode.LeftShift))
-            {
-                Object.Destroy(CommandPattern.Instance.prefabObject);
-                ClearSelection();
-            }
-            CommandPattern.Instance.OnPlace(UseFactoryPattern(mousePosition, CommandPattern.Instance.prefabType));
-        }
-        else {
-
-            #region selection
-            //selection checking
-            if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject())
-            {
-                //Debug.Log(hit.transform.gameObject.name);
-
-                if (hit.collider == null)
-                {
-                    foreach (GameObject obj in SelectedObjects)
-                    {
-                        obj.GetComponent<SelectableObject>().OnDeselect();
-                    }
-                    SelectedObjects.Clear();
-                }
-                else if (currentEvent == MouseEvent.PrefabBuild && Input.GetKey(KeyCode.LeftShift))
-                {
-                    //do nothing
-                }
-                else if (hit.collider != null && hit.transform.gameObject.tag == "SelectableObject")
-                {
-                    //deselect everything else if left control is not holded down
-                    if (!Input.GetKey(KeyCode.LeftShift) && !Input.GetKey(KeyCode.LeftControl))
-                    {
-                        ClearSelection();
-                    }
-                    SelectedObjects.Add(hit.transform.gameObject);
-                    currentEvent = MouseEvent.Selection;
-                    hit.transform.gameObject.GetComponent<SelectableObject>().OnSelect();
-                }
-                //deselect on ground hit
-                else if (hit.collider != null && hit.transform.gameObject.tag == "Ground")
-                {
-                    foreach (GameObject obj in SelectedObjects)
-                    {
-                        obj.GetComponent<SelectableObject>().OnDeselect();
-                    }
-                    currentEvent = MouseEvent.Nothing;
-                    SelectedObjects.Clear();
-                }
-            }
-            else if (currentEvent == MouseEvent.PrefabBuild && Input.GetKeyUp(KeyCode.LeftShift))
-            {
-                Object.Destroy(CommandPattern.Instance.prefabObject);
-                ClearSelection();
-            }
-            #endregion
-
-        }
-
-        #endregion
-
-
-
+        //handle mouse click events last
+        HandleMouseClickEvents();
 
     }
 
@@ -235,7 +95,6 @@ public class SelectionManager : MonoBehaviour
         Building temp;
         switch (type)
         {
-
             case BuildingEnum.BlueBuilding:
                 temp = new BlueBuilding(pos, out returnObj);
                 returnObj.GetComponent<SelectableObject>().type = type;
@@ -259,10 +118,135 @@ public class SelectionManager : MonoBehaviour
 
     }
 
+    private void HandleSelectionBox()
+    {
+        //handle box init behaviour
+        if (Input.GetMouseButtonDown(0) && boxActive == false && currentEvent != MouseEvent.PrefabBuild)
+        {
+            boxStart = Input.mousePosition;
+            boxActive = true;
+        }
+        //handle box drag updates
+        else if (Input.GetMouseButton(0) && boxActive)
+        {
+            if (Mathf.Abs(boxStart.x - Input.mousePosition.x) > 15 || Mathf.Abs(boxStart.y - Input.mousePosition.y) > 15)
+            {
+                boxEnd = Input.mousePosition;
+            }
+            else
+            {
+                boxEnd = Vector2.zero;
+            }
+        }
+        //handle box release
+        if (Input.GetMouseButtonUp(0) && boxActive)
+        {
+            Vector3 worldSelection1;
+
+            Ray rayCast = Camera.main.ScreenPointToRay(boxStart);
+            RaycastHit castHit;
+            if (Physics.Raycast(rayCast, out castHit, 500))
+            {
+                worldSelection1 = castHit.point;
+
+                foreach (GameObject obj in AllObjects)
+                {
+                    if (obj.GetComponent<Transform>().position.x >= Mathf.Min(worldSelection1.x, mousePosition.x) &&
+                        obj.GetComponent<Transform>().position.x <= Mathf.Max(worldSelection1.x, mousePosition.x) &&
+                        obj.GetComponent<Transform>().position.z >= Mathf.Min(worldSelection1.z, mousePosition.z) &&
+                        obj.GetComponent<Transform>().position.z <= Mathf.Max(worldSelection1.z, mousePosition.z) && !SelectedObjects.Contains(obj))
+                    {
+                        Debug.Log(obj.name);
+                        SelectedObjects.Add(obj);
+                        currentEvent = MouseEvent.Selection;
+                        obj.GetComponent<SelectableObject>().OnSelect();
+
+                    }
+                }
+            }
+
+            boxStart = Vector2.zero;
+            boxEnd = Vector2.zero;
+            boxActive = false;
+        }
+    }
+
+    private void HandleMouseClickEvents() {
+
+        //update mouse position on screen
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 500) && hit.transform.gameObject.tag != "SelectableObject")
+        {
+            mousePosition = hit.point;
+            //mousePosition = new Vector3(hit.point.x, 2, hit.point.z);
+        }
+
+        //check if anything needs to be done
+        if (currentEvent == MouseEvent.PrefabBuild && Input.GetMouseButtonDown(0))
+        {
+            if (!Input.GetKey(KeyCode.LeftShift))
+            {
+                Object.Destroy(CommandPattern.Instance.prefabObject);
+                ClearSelection();
+            }
+            CommandPattern.Instance.OnPlace(UseFactoryPattern(mousePosition, CommandPattern.Instance.prefabType));
+        }
+        else
+        {
+
+            //selection checking
+            if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject())
+            {
+                //Debug.Log(hit.transform.gameObject.name);
+
+                if (hit.collider == null)
+                {
+                    foreach (GameObject obj in SelectedObjects)
+                    {
+                        obj.GetComponent<SelectableObject>().OnDeselect();
+                    }
+                    SelectedObjects.Clear();
+                }
+                else if (hit.transform.gameObject.tag == "SelectableObject")
+                {
+                    //deselect everything else if left control is not holded down
+                    if (!Input.GetKey(KeyCode.LeftShift) && !Input.GetKey(KeyCode.LeftControl))
+                    {
+                        ClearSelection();
+                    }
+                    SelectedObjects.Add(hit.transform.gameObject);
+                    currentEvent = MouseEvent.Selection;
+                    hit.transform.gameObject.GetComponent<SelectableObject>().OnSelect();
+                }
+                //deselect on ground selection, with selection exceptions
+                else if (hit.transform.gameObject.tag == "Ground" && !((currentEvent == MouseEvent.PrefabBuild || (currentEvent == MouseEvent.Selection && boxActive)) && Input.GetKey(KeyCode.LeftShift)))
+                {
+                    foreach (GameObject obj in SelectedObjects)
+                    {
+                        obj.GetComponent<SelectableObject>().OnDeselect();
+                    }
+                    currentEvent = MouseEvent.Nothing;
+                    SelectedObjects.Clear();
+                }
+            }
+            //destroy preset on shift hold up
+            else if (currentEvent == MouseEvent.PrefabBuild && Input.GetKeyUp(KeyCode.LeftShift))
+            {
+                Object.Destroy(CommandPattern.Instance.prefabObject);
+                ClearSelection();
+            }
+        }
+    }
+
     private void OnGUI()
     {
+        //used to draw selection box
+
         if (boxStart != Vector2.zero && boxEnd != Vector2.zero) {
             GUI.DrawTexture(new Rect(boxStart.x, Screen.height - boxStart.y, boxEnd.x - boxStart.x, -1 * ((Screen.height - boxStart.y) - (Screen.height - boxEnd.y))), selectionBox);
         }
     }
+
 }
